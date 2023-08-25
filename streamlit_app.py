70640640fac8f02fce805f28c4a2c514705d6968
MountainBean <101438022+MountainBean@users.noreply.github.com>
2023-08-25 12:32:39 +1000

    Update streamlit_app.py
    add comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put it in a table for display
 streamlit.dataframe(fruityvice_normalized)
 
+# import from snowflake
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
