5d95f1fef2f93c6a37927e2a5978c430bd2009d8
MountainBean <101438022+MountainBean@users.noreply.github.com>
2023-08-25 11:22:08 +1000

    Update streamlit_app.py
    removed raw response line

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # take the response and normalise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
