51239668708a4a39444cf35a7cc9add72cfd94a9
MountainBean <101438022+MountainBean@users.noreply.github.com>
2023-08-25 11:21:29 +1000

    Update streamlit_app.py
    normalise and display the fruityvice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# take the response and normalise it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put it in a table for display
+streamlit.dataframe(fruityvice_normalized)
+
